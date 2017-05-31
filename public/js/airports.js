a6ffb07f13dd04ed5a7c85cab7dd024f77134fe0
Ryan Hill <me@ryanjhill.com>
2017-05-31 09:58:50 -0700

    Generate link in info windows to buy tickets, created realistic pin json

--- public/js/airports.js
+++ public/js/airports.js
@@ -1,30 +1,404 @@
 eqfeed_callback(
-  {"airports":[{
-      "type":"Airport",
-      "properties":{
-        "place":"48km SSE of Pondaguitan, Philippines",
-        "time":1348176066,
-        "url":"http://myURL",
-        "price":"$100"
-      },
-      "geometry":{
-        "type":"Point",
-        "coordinates":[126.3832,5.9775]},
-        "id":"usc000csx3"
-      },
-      {
-      "type":"Airport",
-      "properties":{
-        "place":"asdfasg",
-        "time":1348176066,
-        "url":"http://myURL",
-        "price":"$100"
-      },
-      "geometry":{
-        "type":"Point",
-        "coordinates":[150,50]},
-        "id":"usc000csx3"
-      },
-    ]
-  }
+  {"airports":[
+   {
+     "airport": " Amsterdam Airport Schiphol",
+     "place": "Haarlemmermeer, North Holland",
+     "country": "The Netherlands",
+     "iata": "AMS",
+     "coordinates": [52.3086013794, 4.763889789579999],
+     "price": 200
+   },
+   {
+     "airport": " Hartsfield–Jackson Atlanta International Airport",
+     "place": "Atlanta, Georgia",
+     "country": "United States",
+     "iata": "ATL",
+     "coordinates": [33.63669967651367, -84.4281005859375],
+     "price": 1300
+   },
+   {
+     "airport": " Barcelona–El Prat Airport",
+     "place": "El Prat, Catalonia",
+     "country": "Spain",
+     "iata": "BCN",
+     "coordinates": [41.297100067139, 2.0784599781036],
+     "price": 600
+   },
+   {
+     "airport": " Suvarnabhumi Airport",
+     "place": "Bang Phli, Samut Prakan",
+     "country": "Thailand",
+     "iata": "BKK",
+     "coordinates": [13.681099891662598, 100.74700164794922],
+     "price": 1000
+   },
+   {
+     "airport": " Chhatrapati Shivaji International Airport",
+     "place": "Mumbai, Maharashtra",
+     "country": "India",
+     "iata": "BOM",
+     "coordinates": [19.0886993408, 72.8678970337],
+     "price": 1000
+   },
+   {
+     "airport": " Guangzhou Baiyun International Airport",
+     "place": "Baiyun-Huadu, Guangzhou, Guangdong",
+     "country": "China",
+     "iata": "CAN",
+     "coordinates": [23.39240074157715, 113.29900360107422],
+     "price": 1000
+   },
+   {
+     "airport": " Charles de Gaulle Airport",
+     "place": "Roissy-en-France, Île-de-France",
+     "country": "France",
+     "iata": "CDG",
+     "coordinates": [49.0127983093, 2.54999995232],
+     "price": 1000
+   },
+   {
+     "airport": " Soekarno–Hatta International Airport",
+     "place": "Tangerang, Banten",
+     "country": "Indonesia",
+     "iata": "CGK",
+     "coordinates": [-6.1255698204,106.65599823],
+     "price": 200
+   },
+   {
+     "airport": " Charlotte Douglas International Airport",
+     "place": "Charlotte, North Carolina",
+     "country": "United States",
+     "iata": "CLT",
+     "coordinates": [35.2140007019043, -80.94309997558594],
+     "price": 200
+   },
+   {
+     "airport": " Chengdu Shuangliu International Airport",
+     "place": "Shuangliu-Wuhou, Chengdu, Sichuan",
+     "country": "China",
+     "iata": "CTU",
+     "coordinates": [30.578500747680664, 103.9469985961914],
+     "price": 200
+   },
+   {
+     "airport": " Indira Gandhi International Airport",
+     "place": "Delhi",
+     "country": "India",
+     "iata": "DEL",
+     "coordinates": [28.566499710083008, 77.10310363769531],
+     "price": 1300
+   },
+   {
+     "airport": " Denver International Airport",
+     "place": "Denver, Colorado",
+     "country": "United States",
+     "iata": "DEN",
+     "coordinates": [39.861698150635, -104.672996521],
+     "price": 1000
+   },
+   {
+     "airport": " Dallas/Fort Worth International Airport",
+     "place": "Dallas-Fort Worth, Texas",
+     "country": "United States",
+     "iata": "DFW",
+     "coordinates": [32.89680099487305, -97.03800201416016],
+     "price": 850
+   },
+   {
+     "airport": " Hamad International Airport",
+     "place": "Doha",
+     "country": "Qatar",
+     "iata": "DOH",
+     "coordinates": [25.273056, 51.608056],
+     "price": 200
+   },
+   {
+     "airport": " Dubai International Airport",
+     "place": "Garhoud, Dubai",
+     "country": "United Arab Emirates",
+     "iata": "DXB",
+     "coordinates": [25.2527999878,55.3643989563],
+     "price": 1000
+   },
+   {
+     "airport": " Newark Liberty International Airport",
+     "place": "Newark, New Jersey",
+     "country": "United States",
+     "iata": "EWR",
+     "coordinates": [40.692501068115234, -74.168701171875],
+     "price": 850
+   },
+   {
+     "airport": " Leonardo da Vinci–Fiumicino Airport",
+     "place": "Fiumicino, Lazio",
+     "country": "Italy",
+     "iata": "FCO",
+     "coordinates": [41.8002778, 12.2388889],
+     "price": 1000
+   },
+   {
+     "airport": " Frankfurt Airport",
+     "place": "Frankfurt, Hesse",
+     "country": "Germany",
+     "iata": "FRA",
+     "coordinates": [50.0333333,8.5705556],
+     "price": 850
+   },
+   {
+     "airport": " Hong Kong International Airport",
+     "place": "Chek Lap Kok, Islands, Hong Kong",
+     "country": "Hong Kong",
+     "iata": "HKG",
+     "coordinates": [22.3089008331,113.915000916],
+     "price": 1000
+   },
+   {
+     "airport": " Tokyo International Airport",
+     "place": "Ōta, Tokyo",
+     "country": "Japan",
+     "iata": "HND",
+     "coordinates": [35.552299,139.779999],
+     "price": 700
+   },
+   {
+     "airport": " George Bush Intercontinental Airport",
+     "place": "Houston, Texas",
+     "country": "United States",
+     "iata": "IAH",
+     "coordinates": [29.984399795532227, -95.34140014648438],
+     "price": 1300
+   },
+   {
+     "airport": " Seoul Incheon International Airport",
+     "place": "Incheon",
+     "country": "Republic of Korea",
+     "iata": "ICN",
+     "coordinates": [37.46910095214844, 126.45099639892578],
+     "price": 1000
+   },
+   {
+     "airport": " Istanbul Atatürk Airport",
+     "place": "Yeşilköy, Istanbul",
+     "country": "Turkey",
+     "iata": "IST",
+     "coordinates": [40.9768981934, 28.814599990799998],
+     "price": 200
+   },
+   {
+     "airport": " John F. Kennedy International Airport",
+     "place": "Queens, New York City, New York",
+     "country": "United States",
+     "iata": "JFK",
+     "coordinates": [40.63980103, -73.77890015],
+     "price": 1000
+   },
+   {
+     "airport": " Kunming Changshui International Airport",
+     "place": "Guandu, Kunming, Yunnan",
+     "country": "China",
+     "iata": "KMG",
+     "coordinates": [25.1019444,102.9291667],
+     "price": 1400
+   },
+   {
+     "airport": " Kuala Lumpur International Airport",
+     "place": "Sepang, Selangor",
+     "country": "Malaysia",
+     "iata": "KUL",
+     "coordinates": [2.745579957962,101.70999908447],
+     "price": 1000
+   },
+   {
+     "airport": " McCarran International Airport",
+     "place": "Las Vegas, Nevada",
+     "country": "United States",
+     "iata": "LAS",
+     "coordinates": [36.08010101,-115.1520004],
+     "price": 850
+   },
+   {
+     "airport": " Los Angeles International Airport",
+     "place": "Los Angeles, California",
+     "country": "United States",
+     "iata": "LAX",
+     "coordinates": [33.94250107,-118.4079971],
+     "price": 850
+   },
+   {
+     "airport": " Gatwick Airport",
+     "place": "Crawley, West Sussex",
+     "country": "United Kingdom",
+     "iata": "LGW",
+     "coordinates": [51.148101806640625, -0.19027799367904663],
+     "price": 850
+   },
+   {
+     "airport": " Heathrow Airport",
+     "place": "Hillingdon, London",
+     "country": "United Kingdom",
+     "iata": "LHR",
+     "coordinates": [51.4706, -0.461941],
+     "price": 1000
+   },
+   {
+     "airport": " Madrid Barajas Airport",
+     "place": "Barajas, Madrid",
+     "country": "Spain",
+     "iata": "MAD",
+     "coordinates": [40.471926,-3.56264],
+     "price": 200
+   },
+   {
+     "airport": " Orlando International Airport",
+     "place": "Orlando, Florida",
+     "country": "United States",
+     "iata": "MCO",
+     "coordinates": [28.429399490356445, -81.30899810791016],
+     "price": 1000
+   },
+   {
+     "airport": " Benito Juárez International Airport",
+     "place": "Venustiano Carranza, Mexico City",
+     "country": "Mexico",
+     "iata": "MEX",
+     "coordinates": [19.4363, -99.072098],
+     "price": 1000
+   },
+   {
+     "airport": " Miami International Airport",
+     "place": "Miami-Dade County, Florida",
+     "country": "United States",
+     "iata": "MIA",
+     "coordinates": [25.79319953918457,-80.29060363769531],
+     "price": 1000
+   },
+   {
+     "airport": " Ninoy Aquino International Airport",
+     "place": "Pasay/Parañaque, Metro Manila",
+     "country": "Philippines",
+     "iata": "MNL",
+     "coordinates": [14.5086,121.019997],
+     "price": 1500
+   },
+   {
+     "airport": " Minneapolis/St Paul International Airport",
+     "place": "St. Paul, Minnesota",
+     "country": "United States",
+     "iata": "MSP",
+     "coordinates": [44.881999969499994,-93.22180175780001],
+     "price": 700
+   },
+   {
+     "airport": " Munich Airport",
+     "place": "Freising, Bavaria",
+     "country": "Germany",
+     "iata": "MUC",
+     "coordinates": [48.353801727295, 11.786100387573],
+     "price": 350
+   },
+   {
+     "airport": " Narita International Airport",
+     "place": "Narita, Chiba",
+     "country": "Japan",
+     "iata": "NRT",
+     "coordinates": [35.7647018433, 140.386001587],
+     "price": 1000
+   },
+   {
+     "airport": " O'Hare International Airport",
+     "place": "Chicago, Illinois",
+     "country": "United States",
+     "iata": "ORD",
+     "coordinates": [41.97859955,-87.90480042],
+     "price": 1000
+   },
+   {
+     "airport": " Beijing Capital International Airport",
+     "place": "Chaoyang-Shunyi, Beijing",
+     "country": "China",
+     "iata": "PEK",
+     "coordinates": [40.080101013183594, 116.58499908447266],
+     "price": 1000
+   },
+   {
+     "airport": " Phoenix Sky Harbor International Airport",
+     "place": "Phoenix, Arizona",
+     "country": "United States",
+     "iata": "PHX",
+     "coordinates": [33.43429946899414, -112.01200103759766],
+     "price": 200
+   },
+   {
+     "airport": " Shanghai Pudong International Airport",
+     "place": "Pudong, Shanghai",
+     "country": "China",
+     "iata": "PVG",
+     "coordinates": [31.143400192260742,121.80500030517578],
+     "price": 200
+   },
+   {
+     "airport": " Seattle-Tacoma International Airport",
+     "place": "SeaTac, Washington",
+     "country": "United States",
+     "iata": "SEA",
+     "coordinates": [47.44900131225586,-122.30899810791016],
+     "price": 850
+   },
+   {
+     "airport": " San Francisco International Airport",
+     "place": "San Mateo County, California",
+     "country": "United States",
+     "iata": "SFO",
+     "coordinates": [37.61899948120117,-122.375],
+     "price": 200
+   },
+   {
+     "airport": " Shanghai Hongqiao International Airport",
+     "place": "Changning-Minhang, Shanghai",
+     "country": "China",
+     "iata": "SHA",
+     "coordinates": [31.197900772094727,121.33599853515625],
+     "price": 1000
+   },
+   {
+     "airport": " Singapore Changi Airport",
+     "place": "Changi, Singapore",
+     "country": "Singapore",
+     "iata": "SIN",
+     "coordinates": [1.35019, 103.994003],
+     "price": 1050
+   },
+   {
+     "airport": " Sydney Kingsford-Smith Airport",
+     "place": "Mascot, Sydney, New South Wales",
+     "country": "Australia",
+     "iata": "SYD",
+     "coordinates": [-33.94609832763672,151.177001953125],
+     "price": 1000
+   },
+   {
+     "airport": " Shenzhen Bao'an International Airport",
+     "place": "Bao'an, Shenzhen, Guangdong",
+     "country": "China",
+     "iata": "SZX",
+     "coordinates": [22.639299392700195,113.81099700927734],
+     "price": 1000
+   },
+   {
+     "airport": " Taiwan Taoyuan International Airport",
+     "place": "Dayuan, Taoyuan",
+     "country": "Taiwan",
+     "iata": "TPE",
+     "coordinates": [25.0777, 121.233002],
+     "price": 200
+   },
+   {
+     "airport": " Toronto Pearson International Airport",
+     "place": "Mississauga, Ontario",
+     "country": "Canada",
+     "iata": "YYZ",
+     "coordinates": [43.6772003174,-79.63059997559999],
+     "price": 1000
+   }
+  ]}
 );
--- routes/getFlights.js
+++ routes/getFlights.js
@@ -6,43 +6,35 @@ request.get({
   headers: "application/json"
 }, function(err, response, body) {
   skyScan = JSON.parse(body);
-  // console.log(skyScan);
+  console.log(skyScan);
 })
 
-request.get({
-  url: "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/USD/en-US/SAN/anywhere/anytime/?apiKey=sa302839589382574889837967187083",
-  headers: "application/json"
-}, function(err, response, body) {
-  skyScan = JSON.parse(body);
-  // console.log(skyScan);
-})
-
-request.post({
-  url: "http://partners.api.skyscanner.net/apiservices/pricing/v1.0",
-  headers: {
-    Accept: "application/json",
-    ContentType: "application/x-www-form-urlencoded"
-  },
-  form:  {
-    cabinclass: "Economy",
-    country: "UK",
-    currency: "GBP",
-    locale: "en-GB",
-    locationSchema: "iata",
-    originplace: "EDI",
-    destinationplace: "LHR",
-    outbounddate: "2017-05-30",
-    inbounddate: "2017-06-02",
-    adults: "1",
-    children: "0",
-    infants: "0",
-    apikey: "sa822650917824688692115957251692"
-  }},
-  function(err, response, body) {
-    // console.log("Making post request:");
-    // skyScan = JSON.parse(response.headers);
-    // console.log(response.headers);
-    // console.log(body);
-  })
+// request.post({
+//   url: "http://partners.api.skyscanner.net/apiservices/pricing/v1.0",
+//   headers: {
+//     Accept: "application/json",
+//     ContentType: "application/x-www-form-urlencoded"
+//   },
+//   form:  {
+//     cabinclass: "Economy",
+//     country: "UK",
+//     currency: "GBP",
+//     locale: "en-GB",
+//     locationSchema: "iata",
+//     originplace: "EDI",
+//     destinationplace: "LHR",
+//     outbounddate: "2017-05-30",
+//     inbounddate: "2017-06-02",
+//     adults: "1",
+//     children: "0",
+//     infants: "0",
+//     apikey: "sa822650917824688692115957251692"
+//   }},
+//   function(err, response, body) {
+//     console.log("Making post request:");
+//     // skyScan = JSON.parse(response.headers);
+//     console.log(response.headers);
+//     console.log(body);
+//   })
 
 // request.post({url:'http://service.com/upload', form: {key:'value'}}, function(err,httpResponse,body){ /* ... */ })
