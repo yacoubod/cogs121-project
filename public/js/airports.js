9f9c04223b131327ba5bc2c0724c2eada13b528c
yacoubod <yacoubod@gmail.com>
2017-05-30 23:26:09 -0700

    manipulating and restructuring JSON and infowindows

--- public/js/airports.js
+++ public/js/airports.js
@@ -1,29 +1,28 @@
 eqfeed_callback(
-  {"type":"FeatureCollection",
-    "airports":[{
+  {"airports":[{
       "type":"Airport",
       "properties":{
-        "mag":5.4,
         "place":"48km SSE of Pondaguitan, Philippines",
         "time":1348176066,
-        "tz":480,
-        "url":"http://earthquake.usgs.gov/earthquakes/eventpage/usc000csx3",
-        "felt":2,
-        "cdi":3.4,
-        "mmi":null,
-        "alert":null,
-        "status":"REVIEWED",
-        "tsunami":null,
-        "sig":"449",
-        "net":"us",
-        "code":"c000csx3",
-        "ids":",usc000csx3,",
-        "sources":",us,",
-        "types":",dyfi,eq-location-map,general-link,geoserve,historical-moment-tensor-map,historical-seismicity-map,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,tectonic-summary,"
+        "url":"http://myURL",
+        "price":"$100"
       },
       "geometry":{
         "type":"Point",
-        "coordinates":[126.3832,5.9775,111.16]},
+        "coordinates":[126.3832,5.9775]},
+        "id":"usc000csx3"
+      },
+      {
+      "type":"Airport",
+      "properties":{
+        "place":"asdfasg",
+        "time":1348176066,
+        "url":"http://myURL",
+        "price":"$100"
+      },
+      "geometry":{
+        "type":"Point",
+        "coordinates":[150,50]},
         "id":"usc000csx3"
       },
     ]
