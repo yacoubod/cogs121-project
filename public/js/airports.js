ccf286b642bb5d1ad81f81b9eefd97e052de7240
Sandra Luo <shl400@ucsd.edu>
2017-05-31 10:40:01 -0700

    added airports data file

--- public/js/airports.js
+++ public/js/airports.js
@@ -1,6 +1,21 @@
 eqfeed_callback(
   {"airports":[
    {
+    "airport": "Cape Town International Airport",
+    "place": "Cape Town",
+    "country": "South Africa",
+    "iata": "CPT",
+    "coordinates": [-33.9648017883,18.6016998291],
+    "price": 1030
+    },
+    {
+    "airport": "Jorge Chavez International Airport",
+    "place": "Lima",
+    "country": "Peru",
+    "iata": "LIM",
+    "coordinates": [-12.021900177,-77.1143035889],
+   },
+   {
      "airport": " Amsterdam Airport Schiphol",
      "place": "Haarlemmermeer, North Holland",
      "country": "The Netherlands",
